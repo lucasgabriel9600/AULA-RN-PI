cc9280f9198f73da782f39e2d99c978fd0694753
lucasgabriel9600 <130786702+lucasgabriel9600@users.noreply.github.com>
2025-09-25 01:43:08 +0000

    PROJETO2409

---
+++ Tab-Navigation/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import 'react-native-reanimated';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Page</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Settings</Text>
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="- Início -" component={HomeScreen} />
+        <Tab.Screen name="- Configurações -" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
